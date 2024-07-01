ed73b5c15dced82f4d85f855511799a94411b7b1
DoaaMouSSa <doaamoussa5@gmail.com>
2024-07-01 20:03:57 +0300

    finish_clean_arch

---
+++ Restaurant.Data/Entities/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Data.Entities
+{
+    
+    public class Category
+    {
+        public Category()
+        {
+            Items=new HashSet<Item>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Item> Items { get; set; }
+
+    }
+}
---
+++ Restaurant.Data/Entities/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Data.Entities
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        [ForeignKey("CategoryId")]
+        [InverseProperty("Items")]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
